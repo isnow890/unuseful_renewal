7821536e399c9a013557daee7a73ed18b3cad015
양찬우 <isnow890@hotmail.com>
2023-07-05 20:14:04 +0900

    tttt

--- lib/common/model/firestore_props_model.g.dart
+++ lib/common/model/firestore_props_model.g.dart
@@ -26,39 +26,3 @@ Map<String, dynamic> _$FirestorePropsModelToJson(
       'telephoneHistory': instance.telephoneHistory,
       'specimenHistory': instance.specimenHistory,
     };
-
-SearchHistoryTelephoneModel _$SearchHistoryTelephoneModelFromJson(
-        Map<String, dynamic> json) =>
-    SearchHistoryTelephoneModel(
-      lastUpdated: DateTime.parse(json['lastUpdated'] as String),
-      searchValue: json['searchValue'] as String,
-      mode: json['mode'] as String,
-    );
-
-Map<String, dynamic> _$SearchHistoryTelephoneModelToJson(
-        SearchHistoryTelephoneModel instance) =>
-    <String, dynamic>{
-      'lastUpdated': instance.lastUpdated.toIso8601String(),
-      'searchValue': instance.searchValue,
-      'mode': instance.mode,
-    };
-
-SearchHistorySpecimenModel _$SearchHistorySpecimenModelFromJson(
-        Map<String, dynamic> json) =>
-    SearchHistorySpecimenModel(
-      lastUpdated: DateTime.parse(json['lastUpdated'] as String),
-      searchValue: json['searchValue'] as String,
-      mode: json['mode'] as String,
-      startDt: json['startDt'] as String,
-      endDt: json['endDt'] as String,
-    );
-
-Map<String, dynamic> _$SearchHistorySpecimenModelToJson(
-        SearchHistorySpecimenModel instance) =>
-    <String, dynamic>{
-      'lastUpdated': instance.lastUpdated.toIso8601String(),
-      'searchValue': instance.searchValue,
-      'mode': instance.mode,
-      'startDt': instance.startDt,
-      'endDt': instance.endDt,
-    };
